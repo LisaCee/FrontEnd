f353153c8e4b54c7eab04f12aa6fe192e15f1878
LisaCee <lisaceedesign@gmail.com>
2019-09-05 19:02:17 -0700

    navigation working

--- src/Components/NewUser.jsx
+++ src/Components/NewUser.jsx
@@ -3,8 +3,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -55,7 +53,7 @@ export default function SignUp() {
   const classes = useStyles();
 
   return (
-    <Container component='main' maxWidth='xs'>
+    <Container component='main' maxWidth='xs' id='newUser'>
       <CssBaseline />
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
@@ -66,27 +64,14 @@ export default function SignUp() {
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete='fname'
-                name='firstName'
-                variant='outlined'
-                required
-                fullWidth
-                id='firstName'
-                label='First Name'
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <TextField
                 variant='outlined'
                 required
                 fullWidth
-                id='lastName'
-                label='Last Name'
-                name='lastName'
-                autoComplete='lname'
+                id='username'
+                label='Username'
+                name='username'
               />
             </Grid>
             <Grid item xs={12}>
@@ -97,7 +82,6 @@ export default function SignUp() {
                 id='email'
                 label='Email Address'
                 name='email'
-                autoComplete='email'
               />
             </Grid>
             <Grid item xs={12}>
@@ -109,13 +93,17 @@ export default function SignUp() {
                 label='Password'
                 type='password'
                 id='password'
-                autoComplete='current-password'
               />
             </Grid>
             <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value='allowExtraEmails' color='primary' />}
-                label='I want to receive inspiration, marketing promotions and updates via email.'
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='confirmPassword'
+                label='Confirm Password'
+                type='password'
+                id='confirmPassword'
               />
             </Grid>
           </Grid>
@@ -130,7 +118,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify='flex-end'>
             <Grid item>
-              <Link href='#' variant='body2'>
+              <Link href='/signIn' variant='body2'>
                 Already have an account? Sign in
               </Link>
             </Grid>
