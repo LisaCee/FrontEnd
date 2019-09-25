6aee3f44eaa0b44e75c30b896f5dc947b5f30789
LisaCee <lisaceedesign@gmail.com>
2019-09-24 18:29:57 -0700

    added styling back to sign up

--- src/Components/NewUser.jsx
+++ src/Components/NewUser.jsx
@@ -1,49 +1,35 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
 import { register } from "../actions";
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-function Copyright() {
-  return (
-    <Typography variant='body2' color='textSecondary' align='center'>
-      {'Copyright © '}
-      <Link color='inherit' href='https://material-ui.com/'>
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import TextField from "@material-ui/core/TextField";
 
 const styles = theme => ({
-  '@global': {
+  "@global": {
     body: {
       backgroundColor: theme.palette.common.white
     }
   },
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3)
   },
   submit: {
@@ -52,7 +38,6 @@ const styles = theme => ({
 });
 
 export class SignUp extends React.Component {
-
   state = {
     credentials: {
       userName: "",
@@ -61,10 +46,7 @@ export class SignUp extends React.Component {
     },
 
     register: false
-
   };
-
-  // const classes = useStyles();
 
   handleChange = e => {
     this.setState({
@@ -73,6 +55,7 @@ export class SignUp extends React.Component {
         [e.target.name]: e.target.value
       }
     });
+    console.log(this.state);
   };
 
   redirect = () => {
@@ -83,10 +66,10 @@ export class SignUp extends React.Component {
     const creds = {
       userName: this.state.credentials.userName,
       email: this.state.credentials.email,
-      password: this.state.credentials.password,
+      password: this.state.credentials.password
     };
     e.preventDefault();
-    console.log('CREDS', creds)
+    console.log("CREDS", creds);
     this.props.register(creds, this.redirect);
   };
 
@@ -97,99 +80,90 @@ export class SignUp extends React.Component {
     event.preventDefault();
   };
 
-
-
   render() {
-
     const { classes } = this.props;
 
     return (
-
-      // <Container component='main' maxWidth='xs' id='newUser'>
-      //   <CssBaseline />
-      <div>
-        <div className={classes.paper}>
-          {/* <Avatar className={classes.avatar}> */}
-          {/* <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'> */}
-          <h1> Sign up</h1>
-
-          {/* </Typography> */}
-          <form onSubmit={this.signup} className={classes.form} noValidate>
-            <Grid container spacing={2}>
-              {/* <Grid item xs={12}> */}
-              <input
-                variant='outlined'
-                required
-                fullWidth
-                id='username'
-                label='Username'
-                name='username'
-                onChange={this.handleChange}
-              />
-              {/* </Grid>
-            <Grid item xs={12}> */}
-              <input
-                variant='outlined'
-                required
-                fullWidth
-                id='email'
-                label='Email Address'
-                name='email'
-                onChange={this.handleChange}
-              />
-              {/* </Grid>
-            <Grid item xs={12}> */}
-              <input
-                variant='outlined'
-                required
-                fullWidth
-                name='password'
-                label='Password'
-                type='password'
-                id='password'
-                onChange={this.handleChange}
-              />
-              {/* </Grid>
-            <Grid item xs={12}> */}
-              <input
-                variant='outlined'
-                required
-                fullWidth
-                name='confirmPassword'
-                label='Confirm Password'
-                type='password'
-                id='confirmPassword'
-                onChange={this.handleChange}
-              />
+      <Container component="main" maxWidth="xs" id="newUser">
+        <CssBaseline />
+        <div>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <form onSubmit={this.signup} className={classes.form} noValidate>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="userName"
+                    onChange={this.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    onChange={this.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    onChange={this.handleChange}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="confirmPassword"
+                    label="Confirm Password"
+                    type="password"
+                    id="confirmPassword"
+                    onChange={this.handleChange}
+                  />
+                </Grid>
+              </Grid>
+            </form>
+            <Button
+              onClick={this.register}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/signIn" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
             </Grid>
-          </form>
-          <button
-            onClick={this.register}
-            type='submit'
-            fullWidth
-            variant='contained'
-            color='primary'
-          // className={classes.submit}
-          >
-            Sign Up
-          </button>
-          {/* <Grid container justify='flex-end'>
-            <Grid item> */}
-          <Link href='/signIn' variant='body2'>
-            Already have an account? Sign in
-              </Link>
-          {/* </Grid>
-          </Grid> */}
+          </div>
         </div>
-      </div>
-      //   <Box mt={5}>
-      //     <Copyright />
-      //   </Box>
-      //  </div>
-      // </Container>
-
+      </Container>
     );
   }
 }
@@ -203,4 +177,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { register }
-)(withStyles(styles)(SignUp))
+)(withStyles(styles)(SignUp));
